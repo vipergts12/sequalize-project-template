947015e4008a87bda9f8770fdd8ed631ca2a81a1
Oscar Enriquez <macair@Oscars-MacBook-Air.local>
2019-11-09 13:39:56 -0500

    Got delete to work with help from Ryan

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,7 +9,7 @@ const db = require('../models');
 
 // Routes
 // =============================================================
-module.exports = function (app) {
+module.exports = function(app) {
   // GET route for getting all of the todos
   app.get('/api/todos', async (req, res) => {
     // Finding all Chirps, and then returning them to the user as JSON.
@@ -21,7 +21,7 @@ module.exports = function (app) {
   // POST route for saving a new todo.
   // We can create a todo using the data on req.body
   app.post('/api/todos', async (req, res) => {
-    const { text } = req.body;
+    const {text} = req.body;
     const result = await db.todo.create({
       text,
       copmlete: false,
@@ -34,26 +34,31 @@ module.exports = function (app) {
   // We can access the ID of the todo to delete in
   // req.params.id
   app.delete('/api/todos/:id', async (req, res) => {
-    const { text } = req.params.id;
-    const result = await db.todo.create({
-      text,
-      complete: true,
-    });
-    res.send(result);
-
-    // PUT route for updating todos.
-    // We can access the updated todo in req.body
-    app.put('/api/todos', async (req, res) => {
-      const { id, text, complete } = req.body;
-      const result = await db.todo.update(
+    const {id} = req.params;
+    try {
+      await db.todo.destroy({
+        where: {
+          id,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    res.sendStatus(200);
+  });
+  // PUT route for updating todos.
+  // We can access the updated todo in req.body
+  app.put('/api/todos', async (req, res) => {
+    const {id, text, complete} = req.body;
+    const result = await db.todo.update(
         {
           text,
           complete,
         },
         {
-          where: { id },
+          where: {id},
         },
-      );
-      res.send(result);
-    });
-  };
+    );
+    res.send(result);
+  });
+};
